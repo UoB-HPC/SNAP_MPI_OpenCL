a9d8a507d006989e57a9192d78096a19bc96fbce
Tom Deakin <tom.deakin@bristol.ac.uk>
2015-10-06 14:00:59 +0100

    Allocate angular flux buffers

--- src/ocl_buffers.c
+++ src/ocl_buffers.c
@@ -1,8 +1,27 @@
 
 #include "ocl_buffers.h"
 
-
-void allocate_buffers()
+void allocate_buffers(
+    struct problem * problem, struct rankinfo * rankinfo,
+    struct context * context, struct buffers * buffers)
 {
-    
+    cl_int err;
+
+    // Angular flux
+    for (int i = 0; i < 8; i++)
+    {
+        buffers->angular_flux_in[i] = clCreateBuffer(
+            context->context,
+            CL_MEM_READ_WRITE,
+            sizeof(double)*problem->nang*problem->ng*rankinfo->nx*rankinfo->ny*rankinfo->nz,
+            NULL, &err);
+        check_ocl(err, "Creating an angular flux in buffer");
+
+        buffers->angular_flux_out[i] = clCreateBuffer(
+            context->context,
+            CL_MEM_READ_WRITE,
+            sizeof(double)*problem->nang*problem->ng*rankinfo->nx*rankinfo->ny*rankinfo->nz,
+            NULL, &err);
+        check_ocl(err, "Creating an angular flux out buffer");
+    }
 }
